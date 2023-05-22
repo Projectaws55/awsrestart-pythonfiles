87c378b0ac89695e4c05c316e220cfeeef441022
Projectaws55 <134271304+Projectaws55@users.noreply.github.com>
2023-05-22 10:23:11 +0100

    project files aws

---
+++ practice.py
@@ -0,0 +1,16 @@
+myString = "This is a string"
+print(myString)
+print(type(myString))
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firststring = "water"
+secondString = "fall"
+thirdString = firststring+ secondString
+print(thirdString)
+
+name=input("waht is your name")
+print(name)
+color=input("What is your favorite color?")
+animal=input ("What is your favorite  animal?")
+print("{}, you like a {} {}!". format (name , color, animal))
